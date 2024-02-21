21451d6dae1bc3486ffe84800cd5580a43372155
I.Javokhirbek <154643993+MrJavokhirbek@users.noreply.github.com>
2024-02-21 11:05:42 +0900

    3-dars | If else | Nullish

--- index.js
+++ index.js
@@ -1,94 +1,95 @@
-//let a = 1;
-//let b = "2";
+// // console.log(1 == "0" || false);
+// //console.log(1 !== "0" && false "hey");
+// //console.log(! (1 !== "0" && true && "hey"));
 
-//let str1 = 'Future';
-//let str2 = 'Academy';
+// let temp = "sovuq"
+// //0, null, undefined
+// if (temp === "issiq"){                    
+//     console.log("Havo issiq,yengilroq kiyinib oling")
+//     }
+//      else if (temp === "issiq"){
+//         console.log("Havo sovuq,issiqroq kiyinib oling");
+//     }
+//     else if (temp === "yomg'ir"){
+//     console.log("Havo yomg'ir,soyabon olib oling");
+//     }
+//     else if (temp === "qor"){
+//         console.log("Havo qor,qalinroq kiyinib oling ");
+//     }else {
+//         console.log("Nomalum ob havo");
+//     }
+//     if (temp === "sovuq"){
+//         // console.log("nomalum ob havo,sovuq");
+//     }
 
-//console.log(str1 + str2);
-//console.log(isNaN(a));
-//console.log(a + +b)
-//console.log(+b * a)
+// temp = 23; // temp >0 && temp <10, temp >10 && temp <15, temp >15 && temp <25 ;
 
-//let a = "10";
-//let b = "10.3";
+// if (temp < 0) {
+//     console.log("havo juda sovuq");
+// }else if (temp < 10) {
+//     console.log("havo sovuq")
+// }else if (temp < 15) {
+//     console.log("havo issiq");
+// }else if (temp < 25) {
+//     console.log("havo juda issiq");
+// }
 
-//console.log(Number(b));
-//console.log(Number.parseInt(b));
-//console.log(Number.parseFloat(b));
+// temp = 5; // temp >0 && temp <10, temp >10 && temp <15, temp >15 && temp <25 ;
 
-//console.log(false == 0);
-//console.log(true == 1);
-//console.log(false == null);
-//console.log(false == undefined);
-//console.log(undefined == null);
+// if (temp < 0) {
+//     console.log("havo juda sovuq");
+// }else if (temp < 25) {
+//     console.log("havo sovuq")
+// }else if (temp < 15) {
+//     console.log("havo issiq");
+// }else if (temp < 10) {
+//     console.log("havo juda issiq");
+// }
 
-//console.log(true == "0");
-//console.log(false == "0");
+// temp = 25; // temp >0 && temp <10, temp >10 && temp <15, temp >15 && temp <25 ;
 
-//console.log(true == "1");
-//console.log(true == "a");
-//console.log(true == "2");
+// if (temp < 0) 
+//     console.log("havo juda sovuq");
+// else if (temp < 25 && temp >15) 
+//     console.log("havo juda issiq")
+// else if (temp < 15 && temp > 10) 
+//     console.log("havo issiq");
+// else if (temp < 10 && temp >0) 
+//     console.log("havo sovuq");
+// else 
+//     console.log("not found");
 
-//let a = 1;
-//let b = "10";
 
-//console.log(1 + (1 * 1)  /1);
+//  if("condition") console.log("havo juda issiq");
+//  else if ("condition") return true;
+//  else return false;
 
-//let a = 1;
-//console.log(a++);
-//console.log(a);
-//console.log(++a);
+// ? : 
 
-// Obrivations
-//let a = 50;
+// temp <0? === if (temp < 0)
+// temp < 0 
+// ? console.log("havo juda issiq") 
+// : temp < 10
+// ? console.log("havo issiq");
+// : temp < 15
+// ? console.log("havo issiq")
+// : console.log("havo judsa issiq")
 
-//a = a + 10; //50+10=60
-//a *= 10 //60+10=70
+// Nullish ??
 
-//console.log(a);
+// console.log(false || 1);
+// console.log(false ?? 1);
 
-// Comperision
-//let a = 10;
-//let b = 11;
-//let c = "11";
+// console.log(NaN || 1);
+// console.log(NaN ?? 1);
 
-//console.log(a > b);
-//console.log(a<b);
-//console.log(c<b);
-//console.log(c <= b);
-//console.log(c >= b);
-//console.log(c == b);
-//console.log(c === b);
+// console.log(null || 1);
+// console.log(null ?? 1);
 
-//let str1 = "A"
-//let str2 = "a"
+// console.log(undefined || 1);
+// console.log(undefined ?? 1);
 
-//console.log(str1 > str2);
-//console.log(str1 < str2);
-//console.log("str1" < "str2 ");
-
-// let a = 1;
-// let b = 1;
-// let check = true;
-
-//console.log(a == b);
-//console.log(a === b);
-//console.log(a != b);
-//console.log(check);
-//console.log(!check);
-
-// Logical Operations
-// ||OR
-// let ceo = true;
-// let admin = false;
-
-//console.log(ceo === true || admin === true);
-// console.log(true || false);
-// console.log(admin || ceo);
-
-//  && AND
-let id = true;
-let face = false;
-
-console.log(id === true && face === true);
-console.log(id && face);
+console.log(2 * 2);//4
+console.log(2 * 2 *2);//8
+console.log(2 ** 50);
+console.log(2 ** 5);
