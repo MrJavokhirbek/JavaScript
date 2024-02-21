bb8f58dfb64d26c431558e808d145be4317b1d2c
I.Javokhirbek <154643993+MrJavokhirbek@users.noreply.github.com>
2024-02-21 10:46:21 +0900

    JSga kirish |Variables

---
+++ index.js
@@ -0,0 +1,42 @@
+//let name = "Future Academy"
+
+//console.log(`Welcome to ${name} IT Center`);
+//console.log(typeof 1);
+
+//console.log(true);
+//console.log(false);
+//---------------------------------------
+//null 
+
+//let a ;
+//let b = null
+//console.log(null == undefined);
+//console.log(null === undefined);
+//---------------------------------------
+//console.log(null + 1);
+//console.log(undefined + 1);
+
+//let a;
+//let b = null
+//console.log(a + 1);
+//console.log(b + 1);
+
+//console.log(b === b);
+//console.log({b} === {b});
+
+// Symbols
+//let a = Symbol("text");
+//let b = Symbol("text");
+
+//console.log(b === a);
+//console.log(b == a);
+
+// Object
+
+ let ar = ["Michel","Jack?"]; //index
+ let obj = {name: "Future",status: "IT Center"}; // key
+ let func = ()=>{};
+ function test (){
+   console.log(ar[0]);
+   console.log(obj.name);
+}
